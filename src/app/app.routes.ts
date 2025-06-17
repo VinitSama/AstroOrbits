869005e05bd0a5fd63afdb384e0420e49cf29d40
VinitSama <vinitaggarwal08072002@gmail.com>
2025-06-17 17:45:56 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        data: {layout: 'main', breadcrumb: 'Home'},
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./components/home/home').then(m => m.Home),
+                data: { breadcrumb: null },
+            },
+            {
+                path: 'test',
+                loadComponent: () => import('./components/home/home').then(m => m.Home),
+                data: {layout: 'main', breadcrumb: 'test'},
+            },
+            {
+                path: 'horoscope',
+                data: {layout: 'main', breadcrumb: 'Horoscope'},
+                children: [
+                    {
+                        path: '',
+                        loadComponent: () => import('./components/horoscope/horoscope').then(m => m.Horoscope),
+                        data: {breadcrumb: null},
+
+                    }
+                ]
+            }
+        ]
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
+import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    Main,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'AstroOrbits';
+export class App implements OnInit {
+  
+  showLayout: 'main' | 'no-main' = 'main';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const layout = this.getDeepestChild(this.activatedRoute)?.snapshot?.data?.['layout'];
+        this.showLayout = layout === 'main' ? 'main' : 'no-main';
+      });
+  }
+
+  private getDeepestChild(route: ActivatedRoute): ActivatedRoute {
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
+  }
 }
